3d2d7b3663ddade61643fd1e3b4efc52f513550f
Tomspace900 <91191933+Tomspace900@users.noreply.github.com>
2023-11-14 20:43:37 +0100

    Hotfix on App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,4 @@
 function App() {
-	const options = {
-		method: 'GET',
-		headers: {
-			accept: 'application/json',
-			Authorization:
-				'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwZjA1N2EzODE1MTg4MzMyMTBjN2Q3MTU4ZjllOTRkNSIsInN1YiI6IjY1NTNhODJlZWE4NGM3MTA5MjI2YjE3MCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.0B6l3SrOvA-X5xEkZu7YW6fQcUpRZPBdeX5kr517s80',
-		},
-	};
-
-	fetch('https://api.themoviedb.org/3/configuration', options)
-		.then((response) => response.json())
-		.then((response) => console.log(response))
-		.catch((err) => console.error(err));
-
 	return (
 		<div className='flex flex-col justify-between min-h-screen items-center'>
 			<header>
